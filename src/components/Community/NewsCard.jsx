bc3b6236f3b218fd0df31c777fde0342b98dae20
Junseok Ahn <rrvvy9595@gmail.com>
2025-08-17 19:58:11 +0900

    style: 최신 소식에 cursor pointer 추가

--- src/components/Community/NewsCard.jsx
+++ src/components/Community/NewsCard.jsx
@@ -12,12 +12,12 @@ const NewsCard = ({ news }) => {
           {news.title}
         </p>
         <p className="text-xs text-gray-500">{news.date}</p>
-        <div className='border-b border-gray-200 mt-2 mb-2'></div>
+        <div className="border-b border-gray-200 mt-2 mb-2"></div>
       </div>
       <div className="flex justify-between items-center">
         <div className="text-xs text-gray-600">{news.source}</div>
         <div
-          className="w-6 h-6 bg-black rounded-full flex items-center justify-center"
+          className="w-6 h-6 bg-black rounded-full flex items-center justify-center cursor-pointer"
           onClick={() => {
             handleClick(news.url);
           }}
