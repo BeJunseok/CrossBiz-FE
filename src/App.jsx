ead94000bb02d2cb5b161eb7b7148ff8c268c8be
Junseok Ahn <rrvvy9595@gmail.com>
2025-08-25 22:11:04 +0900

    feat: RequireAuth 사용

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
+import RequireAuth from '@/router/RequireAuth';
 import MainLayout from '@/layouts/MainLayout';
 import LayoutWithNavbar from '@/layouts/LayoutWithNavbar';
 import OnboardingPage from '@/pages/onboarding/Onboarding';
@@ -28,9 +29,6 @@ import VisaInfo from './pages/Visa/VisaInfo';
 import VisaHistory from './pages/Visa/VisaHistory';
 import HistoryMatch from './pages/Visa/HistoryMatch';
 import VisaHome from './pages/Visa/VisaHome';
-import ScrollToTop from './components/scrollTop';
-
-
 
 function App() {
   return (
@@ -42,32 +40,35 @@ function App() {
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/register/personal-info" element={<PersonalInfoPage />} />
         <Route path="/register/visa-info" element={<VisaInfoPage />} />
-        <Route path="/community/search" element={<SearchPage />} />
-        <Route path="/community/post/:id" element={<PostDetailPage />} />
-        <Route path="/community/post/new" element={<NewPostPage />} />
-        <Route path="/community/hotpost" element={<HotPostsPage />} />
-        <Route path="/community/my" element={<MyPage />} />
-        <Route path="/community/my/edit" element={<AdditionalInfoPage />} />
-        <Route path="/recommend" element={<RecommendPage />} />
-        <Route path="/schedule/new" element={<NewSchedulePage />} />
-        <Route path="/analysis/:id" element={<LocationTrackingPage />} />
-        <Route path='/loading-prev' element={<LoadingPreviousInfo />} />
-        <Route path='/visa-loading' element={<VisaLoading />} />
-        <Route path='/visa-info' element={<VisaInfo />} />
-        <Route path='/history-match' element={<HistoryMatch />} />
 
-        {/* Navbar가 있는 페이지 */}
-        <Route element={<LayoutWithNavbar />}>
-          <Route path="/community" element={<CommunityPage />} />
-          <Route path="/tax" element={<Tax />} />
-          <Route path="/analysis" element={<MapView />} />
-          <Route path='/' element={<VisaHome />} />
-          <Route path='/confirm-more' element={<ConfirmMore />} />
-          <Route path='/confirm-visa' element={<ConfirmVisa />} />
-          <Route path='/confirm-check' element={<ConfirmCheck />} />
-          <Route path='/visa-recommend' element={<VisaRecommend />} />
-          <Route path='/visa-history' element={<VisaHistory />} />
-        
+        {/* 인증이 필요한 페이지 */}
+        <Route element={<RequireAuth />}>
+          <Route path="/community/search" element={<SearchPage />} />
+          <Route path="/community/post/:id" element={<PostDetailPage />} />
+          <Route path="/community/post/new" element={<NewPostPage />} />
+          <Route path="/community/hotpost" element={<HotPostsPage />} />
+          <Route path="/community/my" element={<MyPage />} />
+          <Route path="/community/my/edit" element={<AdditionalInfoPage />} />
+          <Route path="/recommend" element={<RecommendPage />} />
+          <Route path="/schedule/new" element={<NewSchedulePage />} />
+          <Route path="/analysis/:id" element={<LocationTrackingPage />} />
+          <Route path="/loading-prev" element={<LoadingPreviousInfo />} />
+          <Route path="/visa-loading" element={<VisaLoading />} />
+          <Route path="/visa-info" element={<VisaInfo />} />
+          <Route path="/history-match" element={<HistoryMatch />} />
+
+          {/* Navbar가 있는 페이지 */}
+          <Route element={<LayoutWithNavbar />}>
+            <Route path="/community" element={<CommunityPage />} />
+            <Route path="/tax" element={<Tax />} />
+            <Route path="/analysis" element={<MapView />} />
+            <Route index path="/" element={<VisaHome />} />
+            <Route path="/confirm-more" element={<ConfirmMore />} />
+            <Route path="/confirm-visa" element={<ConfirmVisa />} />
+            <Route path="/confirm-check" element={<ConfirmCheck />} />
+            <Route path="/visa-recommend" element={<VisaRecommend />} />
+            <Route path="/visa-history" element={<VisaHistory />} />
+          </Route>
         </Route>
       </Route>
     </Routes>
