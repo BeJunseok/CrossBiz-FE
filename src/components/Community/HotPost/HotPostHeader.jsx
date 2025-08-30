541d570a4fa09f9580767cb84cd5b324edf9fbd0
Junseok Ahn <rrvvy9595@gmail.com>
2025-08-31 03:21:26 +0900

    feat: HOT글 페이지 i18n 적용

--- src/components/Community/HotPost/HotPostHeader.jsx
+++ src/components/Community/HotPost/HotPostHeader.jsx
@@ -1,7 +1,9 @@
 import { useNavigate } from 'react-router-dom';
 import ChevronLeft from '@/assets/svg/common/ChevronLeft.svg?react';
+import { useTranslation } from 'react-i18next';
 
 const HotPostsHeader = () => {
+  const { t } = useTranslation();
   const nav = useNavigate();
 
   const handleBack = () => {
@@ -10,11 +12,13 @@ const HotPostsHeader = () => {
 
   return (
     <div className="bg-white border-b border-gray-300">
-      <div className="flex items-center gap-28 px-6 py-6">
+      <div className="flex items-center px-6 py-6">
         <button onClick={handleBack} className="p-1">
           <ChevronLeft className="text-gray-600 w-5 h-5" />
         </button>
-        <h1 className="text-black text-xl font-semibold">HOT 글</h1>
+        <h1 className="text-black text-xl font-semibold flex flex-1 justify-center mr-8">
+          {t('community.hotPost.title')}
+        </h1>
       </div>
     </div>
   );
