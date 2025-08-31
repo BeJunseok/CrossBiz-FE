fa4148c4beb44ab574f4e8482e752a5a960ce3d9
Junseok Ahn <rrvvy9595@gmail.com>
2025-08-31 16:41:29 +0900

    style: 최신 글 텍스트 크기 조정

--- src/components/Community/RecentPostItem.jsx
+++ src/components/Community/RecentPostItem.jsx
@@ -47,8 +47,10 @@ const RecentPostItem = ({ post }) => {
         <div className="flex-1">
           <div className="flex justify-between items-start">
             <div className="min-w-0 max-w-52">
-              <p className="text-xs text-black font-medium">{author}</p>
-              <p className="text-xs text-gray-600 font-semibold truncate">
+              <p className="text-[10px] text-black font-medium mb-1">
+                {author}
+              </p>
+              <p className="text-xs text-black font-semibold truncate">
                 {title}
               </p>
             </div>
