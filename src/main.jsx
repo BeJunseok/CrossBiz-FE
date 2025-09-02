1d17a0bd0dfec3dd40eee001707a159d41377c28
Junseok Ahn <rrvvy9595@gmail.com>
2025-09-02 21:39:45 +0900

    feat: Vercel 관련 코드 추가

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,16 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import i18n from './i18n/setting';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
+      <SpeedInsights />
     </BrowserRouter>
   </StrictMode>
 );
